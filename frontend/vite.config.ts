bb087bdd8e76a62650a7d622af8edf9c20db2e46
Manish Khulbe <manishkhulbe123@gmail.com>
2026-01-15 00:11:20 +0530

    added api frontend fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://13.63.44.212",
+        target: "http://13.63.44.212:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
